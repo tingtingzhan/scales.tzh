a2ba3d8b77d57b0b0f58144e506f0e766a2c4bbb
Tingting Zhan <tingtingzhan@gmail.com>
2025-07-25 11:19:53 -0400

    tzh

--- R/label_pvalue_sym.R
+++ R/label_pvalue_sym.R
@@ -1,45 +1,4 @@
 
-#' @title Additional Labeling Functions
-#' 
-#' @description 
-#' Additional labeling `label_*()` functions.
-#' 
-#' @param ... parameters of function \link[scales]{label_number},
-#' other than `scale` and `suffix`
-#' 
-#' @details
-#' To mimic the behavior of function \link[scales]{label_percent}.
-#' 
-#' @note
-#' tzh does not think there is a parameter of function \link[scales]{label_number}
-#' to specify the coding of `NA_real_`.
-#' 
-#' @examples
-#' 3.62e-3 |> label_permille(accuracy = .01)()
-#' c(3.62e-3, NA_real_) |> label_permille(accuracy = .1)()
-#' @name label_
-#' @aliases label_permille
-#' @keywords internal
-#' @importFrom scales label_number
-#' @export
-label_permille <- function(...) {
-  label_number(scale = 1e3, suffix = '\u2030', ...)
-}
-  
-
-#' @rdname label_
-#' @examples
-#' 4.2e-4 |> label_per10thousand(accuracy = .1)()
-#' @aliases label_per10thousand
-#' @importFrom scales label_number
-#' @export
-label_per10thousand <- function(...) {
-  label_number(scale = 1e4, suffix = '\u2031', ...)
-}
-
-
-
-
 
 #' @title Label \eqn{p}-values with Significance Symbol
 #' 
